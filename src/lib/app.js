4d20e0533826a53300ee694ac88382d7071c8493
Eric Lin <ericlin@axiomzen.co>
2015-10-24 12:53:33 -0700

    initial commit, set up base for express app, body parser

---
+++ src/lib/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('../routes')(app);
+
+module.exports = app;
